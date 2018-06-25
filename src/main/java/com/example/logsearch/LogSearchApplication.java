4cc6a34daaeb727e72a00c4658e88a825d6ef449
Anton <30378794+fr0zen87@users.noreply.github.com>
2018-06-25 09:32:45 +0300

    added swagger2

--- src/main/java/com/example/logsearch/LogSearchApplication.java
+++ src/main/java/com/example/logsearch/LogSearchApplication.java
@@ -14,6 +14,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.WebApplicationInitializer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.io.File;
 import java.util.concurrent.Executor;
@@ -22,6 +29,7 @@ import java.util.concurrent.TimeUnit;
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
+@EnableSwagger2
 public class LogSearchApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     public static void main(String[] args) {
@@ -55,5 +63,23 @@ public class LogSearchApplication extends SpringBootServletInitializer implement
                 event -> builder.getReloadingController().checkForReloading(null));
 
         return builder;
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("/.*"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("LogSearch app")
+                .description("Spring REST/SOAP application with Swagger")
+                .version("1.0")
+                .build();
     }
 }
